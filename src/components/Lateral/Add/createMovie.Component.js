131c5e5ef36bd69f313e3c917c810144571d4ad0
DHANI1602 <87883156+DHANI1602@users.noreply.github.com>
2022-12-03 15:23:48 -0300

    Modified CreateMovie.Component

--- src/components/Lateral/Add/createMovie.Component.js
+++ src/components/Lateral/Add/createMovie.Component.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateMovie = () => {
+  const mainTitle = 'Añadir Pelicula';
+
+  const [movieState, setMovieState] = useState({
+    title: '',
+    description: '',
+  });
+  const { title, description } = movieState;
+  const GetFormData = (e) => {
+    e.preventDefault();
+    let target = e.target;
+    let title = target.title.value;
+    let description = target.description.value;
+
+    let movie = {
+      id: new Date().getTime(),
+      title,
+      description,
+    };
+    setMovieState(movie);
+  };
   return (
     <>
       <div className="add">
-        <h3 className="title">Añadir pelicula</h3>
-        <form>
-          <input type="text" placeholder="Titulo" />
-          <textarea placeholder="Descripción"></textarea>
-          <input type="submit" value="guardar" />
+        <h3 className="title">{mainTitle}</h3>
+        <strong>
+          {movieState.title &&
+            movieState.description &&
+            'Has añadido la pelicula'}
+        </strong>
+        <form onSubmit={GetFormData}>
+          <input id="title" name="title " type="text" placeholder="Titulo" />
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Descripción"
+          ></textarea>
+          <input id="save" name="submit" type="submit" value="guardar" />
         </form>
       </div>
     </>
